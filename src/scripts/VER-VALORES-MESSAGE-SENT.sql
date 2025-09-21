5907c30322d7af80555e71699a99d2d4384e76c5
gustausantin <gustausantin@gmail.com>
2025-09-21 20:19:15 +0200

    44

--- src/scripts/VER-VALORES-MESSAGE-SENT.sql
+++
@@ -1,5 +0,0 @@
-SELECT DISTINCT message_sent, COUNT(*) as cantidad
-FROM noshow_actions
-WHERE restaurant_id IN (SELECT id FROM restaurants WHERE name = 'Tavertet')
-GROUP BY message_sent
-ORDER BY cantidad DESC;
--- src/scripts/VERIFICAR-TIPO-MESSAGE-SENT.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    column_name,
-    data_type,
-    is_nullable,
-    column_default
-FROM information_schema.columns
-WHERE table_name = 'noshow_actions'
-AND column_name IN ('message_sent', 'prevented_noshow');
