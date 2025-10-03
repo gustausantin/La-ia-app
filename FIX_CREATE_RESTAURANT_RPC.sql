9dec8cbc9fcf4f09ddd9400b6128fd70da586285
gustausantin <gustausantin@gmail.com>
2025-10-03 19:19:15 +0200

    33

---
+++ FIX_CREATE_RESTAURANT_RPC.sql
@@ -0,0 +1,6 @@
+﻿--  ACTUALIZAR FUNCIÓN create_restaurant_securely
+
+-- Primero eliminar la función existente
+DROP FUNCTION IF EXISTS create_restaurant_securely(JSONB, JSONB);
+
+-- Ahora crear la versión correcta (copia TODO desde la línea 9 hasta el final del archivo 20250128_007_create_restaurant_rpc.sql)
